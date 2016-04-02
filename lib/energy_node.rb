9a8b182fd7d26ea5fbb953a5132de48053b6c857
Leonardo Mateo <leonardomateo@gmail.com>
2016-04-02 16:50:58 -0300

    Add basic energy delivery functionality to EnergyNode

--- lib/energy_node.rb
+++ lib/energy_node.rb
@@ -1,29 +1,44 @@
+require 'json'
 require_relative 'messenger'
+require_relative 'fake_mutex'
 
 class EnergyNode
   include Messenger
 
+  ENERGY_CHUNK = 200
+
   def initialize
     connection.start
-    @channel  = connection.create_channel
-    @exchange = @channel.topic("energy_requests")
-    @queue    = @channel.queue("", :exclusive => true)
+    @channel      = connection.create_channel
+    @requests_xch = @channel.topic("energy_requests")
+    @delivery_xch = @channel.topic("energy_delivery")
+    @queue        = @channel.queue("", :exclusive => true)
   end
 
   def run
-    @queue.bind(@exchange)
+    @queue.bind(@requests_xch)
 
     puts "[#{Time.now.utc.to_s}] Energy Node #{id} running. Press Ctrl+C to exit"
 
     begin
       @queue.subscribe(:block => true) do |info, properties, body|
-        puts "[*] Receiving #{body.inspect}"
-        puts "[INFO] #{info.inspect}"
-        puts "[PROPS] #{properties.inspect}"
+        msg = JSON.parse(body)
+
+        unless FakeMutex.locked?(msg["node_id"])
+          FakeMutex.lock(msg["node_id"]) {
+            puts "[#{Time.now.utc.to_s}] Delivering energy from node #{id} to #{msg["node_id"]}"
+
+            deliver_energy_to(msg["node_id"])
+          }
+        end
       end
     rescue Interrupt => _
       @channel.close
-      @connection.close
+      connection.close
     end
   end
+end
+
+def deliver_energy_to(id)
+  @delivery_xch.publish("200", :routing_key => id)
 end
---
+++ lib/fake_mutex.rb
@@ -0,0 +1,26 @@
+module FakeMutex
+  @register = []
+
+  def self.lock(id, &blk)
+    @register << id
+
+    if block_given?
+      yield
+      sleep 1
+      self.free(id)
+    end
+  end
+
+  def self.locked?(id)
+    @register.include?(id)
+  end
+
+  def self.free(id)
+    @register.delete(id)
+  end
+
+  def register
+    puts @register
+    @register ||= []
+  end
+end
