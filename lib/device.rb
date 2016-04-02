0e0d9d0615cb6ee84211146de1be215e1379dd2e
Leonardo Mateo <leonardomateo@gmail.com>
2016-04-02 18:23:58 -0300

    Make Device aware of energy levels and recharging

--- lib/device.rb
+++ lib/device.rb
@@ -5,17 +5,29 @@ require_relative 'messenger'
 class Device
   include Messenger
 
+  ENERGY_ALERT_LEVEL = 200
+  ENERGY_FULL_LEVEL  = 1000
+
   def initialize
     connection.start
     @channel     = connection.create_channel
     @exchange    = @channel.topic("energy_requests")
     @nrg_xchange = @channel.topic("energy_delivery")
     @queue = @channel.queue("", :exclusive => true)
+    @energy_level = ENERGY_FULL_LEVEL
   end
 
   def request_energy
     @message = { :node_id => id, :message => :need_energy }
     @exchange.publish(@message.to_json)
+  end
+
+  def needs_energy?
+    if @energy_level < ENERGY_ALERT_LEVEL
+      @recharging = true
+    end
+
+    @recharging && @energy_level < ENERGY_FULL_LEVEL
   end
 
   def run
@@ -28,12 +40,15 @@ class Device
     begin
       EventMachine.run do
         #Request energy every 5 seconds
-        timer = EventMachine::PeriodicTimer.new(5) do
-          request_energy
+        timer = EventMachine::PeriodicTimer.new(1) do
+          energy_consuming_task
+
+          request_energy if needs_energy?
         end
 
         @queue.subscribe(:lock => true) do |info, properties, body|
-          log "Receiving #{body}"
+          amount = body.to_i
+          store_energy(amount)
         end
       end
     rescue Interrupt => _
@@ -41,5 +56,23 @@ class Device
       @channel.close
       connection.close
     end
+  end
+
+  private
+  def store_energy(amount)
+    @energy_level += amount
+    log "Charging... #{@energy_level * 100 / ENERGY_FULL_LEVEL}%"
+
+    if @energy_level > ENERGY_FULL_LEVEL
+      @energy_level = ENERGY_FULL_LEVEL
+      @recharging = false
+      log "Charged!"
+    end
+  end
+
+  def energy_consuming_task
+    log "Discharging... #{@energy_level * 100 / ENERGY_FULL_LEVEL}%" unless @recharging
+    @energy_level -= rand(20..35)
+    raise "Out Of Energy" if @energy_level <= 0
   end
 end
